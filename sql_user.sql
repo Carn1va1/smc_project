3fbe0b173dca40a2f65f1c20b1649de5564efff6
Carn1va1 <leejm030626@gmail.com>
2020-11-11 15:03:43 +0900

    sql 공부
    sql 쿼리문을 사용 하였음.

--- sql_user.sql
+++ sql_user.sql
@@ -1,3 +1,4 @@
+-- 2020.11.10
 CREATE USER SMC_USER IDENTIFIED BY SMC_USER;
 
 GRANT CREATE SESSION TO SMC_USER;
@@ -41,3 +42,78 @@ INSERT INTO A(A) VALUES ('ABCTEST');
 SELECT * FROM A;
 
 DROP TABLE A;
+
+-- 2020.11.11
+CREATE TABLE PRODUCT (
+    id number NOT NULL,
+    category_Id number NOT NULL,
+    name varchar2(200) NOT NULL,
+    price number NOT NULL,
+    stock number DEFAULT 0,
+    description varchar2(200),
+    origin varchar2(200),
+    CONSTRAINT PRODUCT_PK PRIMARY KEY (id)
+);
+
+CREATE TABLE CATEGORY (
+    id number NOT NULL,
+    name varchar2(200) NOT NULL,
+    CONSTRAINT CATEGORY_PK PRIMARY KEY (id)
+);
+
+CREATE TABLE MEMBER (
+    id number NOT NULL,
+    password varchar2(100) NOT NULL,
+    name varchar2(50) NOT NULL,
+    email varchar2(100) NOT NULL,
+    phone varchar2(40),
+    address varchar2(200),
+    age number(3),
+    CONSTRAINT MEMBER_PK PRIMARY KEY (id)
+);
+
+CREATE TABLE PAYMENT_HISROTY (
+    id number NOT NULL,
+    order_date date NOT NULL,
+    order_status varchar2(16) NOT NULL, -- PAYMENT_WAITING, PATMENT_SUCCESS, SHIPPING, SHIPPED
+    order_count number NOT NULL,
+    order_price number NOT NULL,
+    product_id number NOT NULL,
+    member_id number NOT NULL,
+    address varchar2(200) NOT NULL,
+    CONSTRAINT PAYMENT_HISROTY_PK PRIMARY KEY (id),
+    CONSTRAINT PAYMENT_HISROTY_CK CHECK (order_status IN('PAYMENT_WAITING', 'PAYMENT_SUCCESS', 'SHIPPING', 'SHIPPED'))
+);
+
+COMMENT ON COLUMN PAYMENT_HISROTY.ORDER_STATUS IS '주문 진행 단계, 4가지 단계만 들어갈 수 있도록 함';
+
+SELECT *
+FROM ALL_COL_COMMENTS
+WHERE TABLE_NAME = 'PAYMENT_HISTORY'
+
+SELECT * FROM PRODUCT WHERE;
+
+INSERT INTO PRODUCT(id, category_Id, name, price, stock, description, origin)
+VALUES(49, 1, '2021년식 맥북', 1690000, 10, '애플 그 이상', '캘리포니아');
+
+-- CATALOGUE 테이블을 만든다.
+CREATE TABLE CARTALOGUE (
+    id number NOT NULL,
+    name varchar2(200) NOT NULL,
+    price number NOT NULL,
+    origin varchar2(200)
+);
+
+-- PRODUCT에 있는 100000원 이상을 CARTALOGUE에 추가한다.
+INSERT INTO CARTALOGUE
+SELECT id, name, price, origin
+FROM PRODUCT
+WHERE price >= 100000;
+
+-- PRODUCT 중에 100000원 이상이면 STOCK 5개를 추가한다.
+update PRODUCT
+SET stock = stock + 5
+WHERE price >= 100000;
+
+-- PRODUCT ID 49를 삭제한다.
+DELETE PRODUCT WHERE ID=49;
